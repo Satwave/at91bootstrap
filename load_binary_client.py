bf13fccccb01249657945356fb3110d89d3622a9
NikolayTranakiev <ntranakiev@satwave.ai>
2026-03-11 13:52:55 -0400

    fixed client server zmq comms

--- load_binary_client.py
+++ load_binary_client.py
@@ -1,22 +1,35 @@
 import zmq
 import sys
 import os
-import time
+import time 
+from datetime import datetime
+
+BLOCK_SIZE = 16384
 
 context = zmq.Context()
-socket = context.socket(zmq.PUSH)
+socket = context.socket(zmq.PAIR)
 
-socket.connect("tcp://192.168.5.83:5555")
+socket.connect("tcp://192.168.5.83:5001")
+time.sleep(1)
 
 filepath = sys.argv[1]
-filename = "ACU_firmware_" + str(int(time.time())) + ".bin"
+
+timestamp = os.path.getmtime(filepath)
+time_str = datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d_%H:%M:%S")
+
+filename = "ACU_firmware_" + time_str + ".bin"
+socket.send_string(filename)
+
+filesize = os.path.getsize(filepath)
+print(f"Sending {filename}, size: {filesize} bytes")
+socket.send(filesize.to_bytes(8, byteorder='big'))
+
+data = []
 
 with open(filepath, "rb") as f:
-    data = f.read()
-
-socket.send_multipart([
-    filename.encode(),
-    data
-])
-
-print(f"Sent {filename}")
+    while True:
+        chunk = f.read(BLOCK_SIZE)
+        if not chunk:
+            break
+        socket.send(chunk) 
+        time.sleep(0.01) 
--- load_binary_server.py
+++ load_binary_server.py
@@ -1,37 +1,37 @@
 import zmq
 import time
+import os
 
 from load_binary import binaryLoad
 
 
 if __name__ == "__main__":
     context = zmq.Context()
-    socket = context.socket(zmq.PULL)
-    socket.bind("tcp://*:5555")
+    socket = context.socket(zmq.PAIR)
+    socket.bind("tcp://*:5001")
+    time.sleep(0.1)
 
     acu_instance = binaryLoad("/dev/ttyUSB0")
+    bytes_read = 0
 
     while True:
-        filename, data = socket.recv_multipart()
 
-        filename = filename.decode()
+        filename = socket.recv_string()
+        print("filename recv", filename)
+
+        filesize = int.from_bytes(socket.recv(), byteorder='big')
+        print("file size", filesize)
+
+        if os.path.exists(filename):
+            os.remove(filename)
+            print(f"Removed old file: {filename}")
+
 
         with open(filename, "wb") as f:
-            f.write(data)
+            while bytes_read < filesize:
+                packet = socket.recv()
+                bytes_read += len(packet)
+                f.write(packet)
 
-        print(f"Received file: {filename} ({len(data)} bytes)")
 
         acu_instance.load(filename)
-
-
-
-
-
-    data = socket.recv()
-
-    filename = "ACU_bin_" + str(int(time.time())) + ".bin"
-
-    with open("received.bin", "wb") as f:
-        f.write(data)
-
-    print("File received")
